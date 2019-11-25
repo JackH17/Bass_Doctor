0d55d47f1dc538f3533181c6b2497da943393b54
JackH17 <jack.hawkins77@googlemail.com>
2019-11-25 21:42:37 +0000

    doctor component

--- src/Containers/TheDoctor.js
+++ src/Containers/TheDoctor.js
@@ -1,20 +1,34 @@
 import React, { useState } from 'react'
-
+///////////////////////////////////////////
 import { getUserMediaStream } from '../utils/GetUserInput';
+import { distortionCurve } from '../utils/DistortionCurve';
+///////////////////////////////////////////
+import Comp from '../Components/Comp';
 
 const TheDoctor = () => {
 
     const [audioCTX, setAudioCTX] = useState();
 
-    const [analyser, setAnalyser] = useState();
-
     const [input, setInput] = useState();
+
+    const [fxChain, setFXChain] = useState({});
 
     const [dryGain, setDryGain] = useState();
     const [wetGain, setWetGain] = useState();
 
     const [output, setOutput] = useState();
 
+    const setComp = (comp) => {
+
+        fxChain.comp.attack.value = comp.attack;
+        fxChain.comp.release.value = comp.release;
+        fxChain.comp.knee.value = comp.knee;
+        fxChain.comp.ratio.value = comp.ratio;
+        fxChain.comp.threshold.value = comp.threshold;
+
+        console.log(fxChain)
+    
+    }
 
 
     const setUserInputs = (streamNode, dryGainNode, wetGainNode) => {
@@ -25,31 +39,62 @@ const TheDoctor = () => {
 
     }
 
+    const setEffectsNodes = async (comp, filter, dist) => {
+
+        filter.type = 'bandpass';
+        filter.frequency.value = 500;
+        filter.Q.value = 5;
+        filter.gain.value = 1;
+
+        console.log(filter);
+
+       comp.threshold.value = -20;
+
+       comp.attack.value = 0.001;
+       comp.release.value = 0.025;
+
+       console.log(dist)
+
+
+
+       const curve = await distortionCurve(50);
+
+       dist.curve = curve;
+
+
+        setFXChain({
+            comp,
+            filter, 
+            dist
+        });
+
+    }
+
 
     const getContext = async () => {
 
         const AudioContext = window.AudioContext || window.webkitAudioContext;
         const context =  new AudioContext();
-        const analyser = context.createAnalyser();
-
 
         setAudioCTX(context);
         setOutput(context.destination);
-        setAnalyser(analyser);
-
-
+        
         const userMediaStreamNode = await getUserMediaStream(context);
 
-
-
         setUserInputs(userMediaStreamNode, context.createGain(), context.createGain());
+
+
+        setEffectsNodes(context.createDynamicsCompressor(), context.createBiquadFilter(), context.createWaveShaper());
     };
 
     const engageDoctor = () => {
 
+        console.log(fxChain)
+
+        console.log(wetGain)
+
         input.connect(dryGain).connect(output);
-        input.connect(wetGain).connect(output);
-        input.connect(analyser)
+        input.connect(wetGain).connect(fxChain.comp).connect(fxChain.filter).connect(fxChain.dist).connect(output);
     }
 
 
@@ -59,6 +104,7 @@ const TheDoctor = () => {
             <h1>The Doctor</h1>
             <button onClick={getContext}>get input</button>
             <button onClick={engageDoctor}>engage</button>
+            <Comp setComp={setComp}/>
  
         </div>
     )
